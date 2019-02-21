46e4b22d93f21143a10ecc9f6c4be675935f93d8
Alexander Pehm <alexander@pehm.biz>
2019-02-21 14:23:05 +0100

    Removed todotxt file serving

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 var express = require('express');
 var app = express();
 
-const todoTxtFilePath = '/Users/michalex/todo_ptpp.txt'
-
-
 app.use(express.static('dist'));
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/dist/index.html');
-});
-
-app.get("/todos", function(request, response) {
-  response.sendFile(todoTxtFilePath);
 });
 
 var listener = app.listen(8000, function () {
